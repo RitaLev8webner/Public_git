de3e11423da4a6e3e6c347086d6e69beb5f7bd33
rsharma15 <rita_sharma@intuit.com>
2020-01-06 10:55:02 +0530

    comm-123 test project.

---
+++ src/main/java/com/Test1.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        String[] S = {
+                "https://www.hackerearth.com",
+                "https://www.wikipedia.org",
+                "https://www.google.com",
+                "https://www.hackerearth.com",
+                "https://www.hackerearth.com"};
+
+
+
+
+    }
+}
